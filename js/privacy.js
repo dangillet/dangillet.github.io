a3e72312a6ec1e6ad416f02dbf5b3a506df890c4
Daniel Gillet <daniel.gillet@flightdataservices.com>
2020-06-28 13:57:58 +0200

    More twekaing for faster page loading

--- js/privacy.js
+++ js/privacy.js
@@ -1,14 +1,5 @@
-function docReady(fn) {
-    // see if DOM is already available
-    if (document.readyState === "complete" || document.readyState === "interactive") {
-        // call on next available tick
-        setTimeout(fn, 1);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
-    }
-}
 
-docReady(function () {
+$(function () {
     $.ajax({
         url: "privacy.html",
     })
