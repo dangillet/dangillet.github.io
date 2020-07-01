d0d6f88eb9e42aebd56bed219c3fc19515d06186
Daniel Gillet <daniel.gillet@flightdataservices.com>
2020-07-01 17:07:33 +0100

    Added crossorigin for Google Map loading

--- js/lib.js
+++ js/lib.js
@@ -26,6 +26,7 @@ $(function() {
     const script = document.createElement('script');
     script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDC5qb4_2EaotSUpCpWyspjOrRQBezmG-0&callback=initMap';
     script.async = true;
+    script.crossOrigin = 'anonymous'
 
     // Attach your callback function to the `window` object
     window.initMap = initMap;
